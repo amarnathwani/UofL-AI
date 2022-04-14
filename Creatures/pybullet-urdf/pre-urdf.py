2740a381170eb8a3d8d76e10cd0a0a4fe4535c28
amar <amar3734@gmail.com>
2022-04-14 14:47:10 +0300

    premade model/udf file

---
+++ Creatures/pybullet-urdf/pre-urdf.py
@@ -0,0 +1,19 @@
+import pybullet as p 
+import pybullet_data
+import time
+
+p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+p.createCollisionShape(p.GEOM_PLANE) # the floor
+p.createMultiBody(0, 0)
+
+ur_file = 'r2d2.urdf'
+creatureId = p.loadURDF(ur_file)
+
+p.setGravity(0, 0, -10)
+p.setRealTimeSimulation(1)
+
+while True:
+    p.stepSimulation()
+    time.sleep(1.0/240)
